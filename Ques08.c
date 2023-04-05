53016277ae6a2cba63a179ecd9166b62b6d6e528
Sarwar Ahammed <110556391+sarwar1ahammed@users.noreply.github.com>
2023-04-05 22:59:22 +0530

    Create Ques08.c

---
+++ Ques08.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+// Print or display an upper triangular matrix.
+int main()
+{
+    int r, c, i, j;
+    int a[10][10];
+    printf("Enter rows no. : ");
+    scanf("%d", &r);
+    printf("Enter columns no. : ");
+    scanf("%d", &c);
+    printf("Enter matrix elements : \n");
+    for (i=0; i<r; i++)
+    {
+        for (j=0; j<c; j++)
+        {
+            printf("Element - [%d],[%d] : ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+    printf("Matrix is : \n");
+    for (i=0; i<r; i++)
+    {
+        for (j=0; j<c; j++)
+            printf("%3d", a[i][j]);
+        printf("\n");
+    }
+    printf("Upper Triangular Matrix is : \n");
+    for (i=0; i<r; i++)
+    {
+        for (j=0; j<c; j++)
+        {
+            if (j>=i)
+                printf("%3d", a[i][j]);
+            else 
+                printf("   ");
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+    return 0;
+}
