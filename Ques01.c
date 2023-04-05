a1c1553443c15c7b01b1aead570bcaa2af768931
Sarwar Ahammed <110556391+sarwar1ahammed@users.noreply.github.com>
2023-04-05 22:53:59 +0530

    Create Ques01.c

---
+++ Ques01.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+// Calculate the sum of two matrices each of order 3x3.
+int main()
+{
+    int i, j;
+    int a[3][3], b[3][3], sum[3][3];
+    printf("Enter 1st 3x3 matrix elements : \n");
+    for (i=0; i<3; i++)
+    {
+        for (j=0; j<3; j++)
+        {
+            printf("Element - [%d],[%d] : ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+    printf("1st Matrix is : \n");
+    for (i=0; i<3; i++)
+    {
+        for (j=0; j<3; j++)
+            printf("%3d", a[i][j]);
+        printf("\n");
+    }
+    printf("Enter 2nd 3x3 matrix elements : \n");
+    for (i=0; i<3; i++)
+    {
+        for (j=0; j<3; j++)
+        {
+            printf("Element - [%d],[%d] : ", i, j);
+            scanf("%d", &b[i][j]);
+        }
+    }
+    printf("2nd Matrix is : \n");
+    for (i=0; i<3; i++)
+    {
+        for (j=0; j<3; j++)
+            printf("%3d", b[i][j]);
+        printf("\n");
+    }
+    printf("Sum of two matrics is : \n");
+    for (i=0; i<3; i++)
+    {
+        for (j=0; j<3; j++)
+        {
+            sum[i][j] = a[i][j] + b[i][j];
+            printf("%3d", sum[i][j]);      
+        }
+        printf("\n");
+    }
+    
+
+    printf("\n");
+    return 0;
+}
